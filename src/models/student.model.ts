3a37b35d14f9fab6e00a2449ee13dc54f1b79082
vamshiInqui-lab <97531883+vamshiInqui-lab@users.noreply.github.com>
2022-10-21 19:06:52 +0530

    Student get profile

--- src/models/student.model.ts
+++ src/models/student.model.ts
@@ -7,6 +7,7 @@ import { baseConfig } from '../configs/base.config';
 import { user } from './user.model';
 import { admin } from './admin.model';
 import { mentor } from './mentor.model';
+import { team } from './team.model';
 
 export class student extends Model<InferAttributes<student>, InferCreationAttributes<student>> {
     declare student_id: CreationOptional<number>;
@@ -146,5 +147,7 @@ student.init(
 
 // student.belongsTo(user, { foreignKey: 'user_id', constraints: false });
 // user.hasOne(student, { foreignKey: 'user_id', constraints: false, scope: { role: 'STUDENT' } });
+student.belongsTo(user, { foreignKey: 'user_id' });
+user.hasMany(student, { foreignKey: 'user_id' });
 student.belongsTo(user, { foreignKey: 'user_id' });
 user.hasMany(student, { foreignKey: 'user_id' });
