c2ae5beb66c66e92f48037bb78276f19c753ddfd
vamshiInqui-lab <97531883+vamshiInqui-lab@users.noreply.github.com>
2022-10-22 16:05:42 +0530

    Addded a new API

--- src/controllers/user.controler.ts
+++ src/controllers/user.controler.ts
@@ -5,10 +5,11 @@ import dispatcher from '../utils/dispatch.util';
 import authService from '../services/auth.service';
 import BaseController from './base.controller';
 import ValidationsHolder from '../validations/validationHolder';
+import { badRequest } from 'boom';
 
 export default class UserController extends BaseController {
     model = "user";
-
+    authService: authService = new authService;
     protected initializePath(): void {
         this.path = '/users';
     }
@@ -17,7 +18,18 @@ export default class UserController extends BaseController {
     }
     protected initializeRoutes(): void {
         //example route to add
-        //this.router.get(`${this.path}/`, this.getData);
+        this.router.put(`${this.path}/updateMentorDetails`, this.updateMentorUserDetails.bind(this));
         super.initializeRoutes();
     }
+
+    private async updateMentorUserDetails(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+        try {
+            const result = await this.authService.updateUserMentorDetails(req.body);
+            if (!result) throw res.status(404).send(dispatcher(res, null, 'error', speeches.USER_NOT_FOUND));
+            else if (result.error) return res.status(404).send(dispatcher(res, result.error, 'error', result.error));
+            else return res.status(202).send(dispatcher(res, result.data, 'accepted', speeches.USER_PASSWORD_CHANGE, 202));
+        } catch (error) {
+            next(error)
+        }
+    };
 };
