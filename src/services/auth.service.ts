b1120c2b314cae0b11de12e65e9a9baf0186a2e4
vamshiInqui-lab <97531883+vamshiInqui-lab@users.noreply.github.com>
2022-11-11 16:29:50 +0530

    Added mentor_id to response

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -48,6 +48,7 @@ export default class authService {
                 include: {
                     model: mentor,
                     attributes: [
+                        "mentor_id",
                         'user_id',
                         'full_name',
                         'mobile',
