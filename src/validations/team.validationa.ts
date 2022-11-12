1e20629a63eda34f6e9a21bad8039c9ae62d278f
vamshiInqui-lab <97531883+vamshiInqui-lab@users.noreply.github.com>
2022-11-12 12:43:07 +0530

    name validations updated

--- src/validations/team.validationa.ts
+++ src/validations/team.validationa.ts
@@ -10,19 +10,17 @@ import { speeches } from '../configs/speeches.config';
 //         'string.empty': speeches.DESCRIPTION_REQUIRED
 //     }),
 // });
-
-const alphaNumPattern = /^[a-zA-Z0-9 ]*$/;
 export const teamSchema = Joi.object().keys({
     mentor_id: Joi.string().trim().min(1).required().messages({
         'string.empty': speeches.ID_REQUIRED
     }),
-    team_name: Joi.string().trim().min(1).regex(alphaNumPattern).required().messages({
+    team_name: Joi.string().trim().min(1).regex(constents.ALPHA_NUMERIC_PATTERN).required().messages({
         'string.empty': speeches.NAME_REQUIRED
     })
 });
 export const teamUpdateSchema = Joi.object().keys({
     status: Joi.string().trim().min(1).valid(...Object.values(constents.common_status_flags.list)).required(),
-    team_name: Joi.string().trim().min(1).regex(alphaNumPattern).required().messages({
+    team_name: Joi.string().trim().min(1).regex(constents.ALPHA_NUMERIC_PATTERN).required().messages({
         'string.empty': speeches.NAME_REQUIRED
     })
 });
