2ae1ac035ee4482244eda6dcf4bac954dbbe785d
vamshiInqui-lab <97531883+vamshiInqui-lab@users.noreply.github.com>
2022-11-12 12:03:22 +0530

    Team Validations

--- src/validations/team.validationa.ts
+++ src/validations/team.validationa.ts
@@ -11,18 +11,18 @@ import { speeches } from '../configs/speeches.config';
 //     }),
 // });
 
+const alphaNumPattern = /^[a-zA-Z0-9 ]*$/;
 export const teamSchema = Joi.object().keys({
-    mentor_id: Joi.string().required().messages({
+    mentor_id: Joi.string().trim().min(1).required().messages({
         'string.empty': speeches.ID_REQUIRED
     }),
-    team_name: Joi.string().required().messages({
+    team_name: Joi.string().trim().min(1).regex(alphaNumPattern).required().messages({
         'string.empty': speeches.NAME_REQUIRED
     })
 });
-
 export const teamUpdateSchema = Joi.object().keys({
-    status: Joi.string().valid(...Object.values(constents.common_status_flags.list)).required(),
-    team_name: Joi.string().required().messages({
+    status: Joi.string().trim().min(1).valid(...Object.values(constents.common_status_flags.list)).required(),
+    team_name: Joi.string().trim().min(1).regex(alphaNumPattern).required().messages({
         'string.empty': speeches.NAME_REQUIRED
     })
 });
