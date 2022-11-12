d148740577956c2c6ff3589bf63412a0d382a1d7
amn <satija.aman@gmail.com>
2022-11-12 14:09:13 +0530

    comitting indexs to users teams and course_topics

--- src/database/migrations/migrations/2022.11.12T08.19.15.add_indexes.ts
+++ src/database/migrations/migrations/2022.11.12T08.19.15.add_indexes.ts
@@ -7,13 +7,30 @@ export const tableName = "name_of_your_table";
 export const up: Migration = async ({ context: sequelize }) => {
 	// await sequelize.query(`raise fail('up migration not implemented')`); //call direct sql 
 	//or below implementation 
-	await sequelize.getQueryInterface().createTable(tableName, {
-		teams_courses_mappings_id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        },
-	  });
+	const transaction = await sequelize.getQueryInterface().sequelize.transaction();
+  try{
+    //users table 
+    await sequelize.getQueryInterface().addIndex("users", ['username'], { 
+		name: 'IDX_USR_UN',
+		unique:true,
+		transaction });
+
+	//teams table
+	await sequelize.getQueryInterface().addIndex("teams", ['mentor_id',"name"], { 
+		name: 'UNQ_TEAM_NAME',
+		unique:true,
+	  	transaction });
+    
+	//course_topics table
+	await sequelize.getQueryInterface().addIndex("course_topics", ['topic_type'], { name: 'IDX_CTOP_CTOPTYPE',transaction });
+	await sequelize.getQueryInterface().addIndex("course_topics", ['topic_type_id'], { name: 'IDX_CTOP_CTOPTYPEID',transaction });
+
+    await transaction.commit();
+  }catch(err){
+      await transaction.rollback();
+      throw err;
+  }
+	
 };
 
 export const down: Migration = async ({ context: sequelize }) => {
