0a1b1af90a8998bdb92672e782bc5b99c467916d
vamshiInqui-lab <97531883+vamshiInqui-lab@users.noreply.github.com>
2022-12-08 16:44:53 +0530

    Update organization.validations.ts

--- src/validations/organization.validations.ts
+++ src/validations/organization.validations.ts
@@ -38,6 +38,21 @@ export const organizationUpdateSchema = Joi.object().keys({
         'any.only': speeches.COMMON_STATUS_INVALID,
         'string.empty': speeches.COMMON_STATUS_REQUIRED
     }),
+    organization_code: Joi.string().trim().min(1).required().messages({
+        'string.empty': speeches.ORG_CODE_REQUIRED
+    }),
+    organization_name: Joi.string().trim().min(1).required().messages({
+        'string.empty': speeches.ORG_NAME_REQUIRED
+    }),
+    district: Joi.string().messages({
+        'string.empty': speeches.DISTRICT_REQ
+    }),
+    principal_name: Joi.string(),
+    principal_mobile: Joi.string(),
+    principal_email: Joi.string(),
+    city: Joi.string(),
+    state: Joi.string(),
+    country: Joi.string()
 });
 export const organizationCheckSchema = Joi.object().keys({
     organization_code: Joi.string().required().messages({
