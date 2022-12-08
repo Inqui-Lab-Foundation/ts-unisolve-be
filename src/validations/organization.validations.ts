8fbf05ffdda245ea0b69ff881d481b6de6219c8b
vamshiInqui-lab <97531883+vamshiInqui-lab@users.noreply.github.com>
2022-12-08 16:12:26 +0530

    Update organization.validations.ts

--- src/validations/organization.validations.ts
+++ src/validations/organization.validations.ts
@@ -21,27 +21,15 @@ export const organizationRawSchema = Joi.object().keys({
     organization_name: Joi.string().trim().min(1).required().messages({
         'string.empty': speeches.ORG_NAME_REQUIRED
     }),
-    principal_name: Joi.string().messages({
-        'string.empty': speeches.PRINCIPAL_NAME_REQ
-    }),
-    principal_mobile: Joi.string().messages({
-        'string.empty': speeches.PRINCIPAL_MOBILE_REQ
-    }),
-    principal_email: Joi.string().messages({
-        'string.empty': speeches.PRINCIPAL_EMAIL_REQ
-    }),
-    city: Joi.string().messages({
-        'string.empty': speeches.CITY_REQ
-    }),
     district: Joi.string().messages({
         'string.empty': speeches.DISTRICT_REQ
     }),
-    state: Joi.string().messages({
-        'string.empty': speeches.STATE_REQ
-    }),
-    country: Joi.string().messages({
-        'string.empty': speeches.CITY_REQ
-    }),
+    principal_name: Joi.string(),
+    principal_mobile: Joi.string(),
+    principal_email: Joi.string(),
+    city: Joi.string(),
+    state: Joi.string(),
+    country: Joi.string(),
     status: Joi.string().valid(...Object.values(constents.organization_status_flags.list))
 });
 
