e547bd4dc5ee05f0f797f108035c2ae799f44a1b
vamshiInqui-lab <97531883+vamshiInqui-lab@users.noreply.github.com>
2022-12-13 15:40:20 +0530

    Update challenge_rating.controller.ts

--- src/controllers/challenge_rating.controller.ts
+++ src/controllers/challenge_rating.controller.ts
@@ -1,14 +1,19 @@
+import { challengeRatingSchema, challengeRatingUpdateSchema } from "../validations/challenge_rating.validations";
 import ValidationsHolder from "../validations/validationHolder";
 import BaseController from "./base.controller";
 
 export default class ChallengeRatingController extends BaseController {
+
     model = "challenge_rating";
+
     protected initializePath(): void {
         this.path = '/challengeRating';
     }
+
     protected initializeValidations(): void {
-        this.validations = new ValidationsHolder(null, null);
+        this.validations = new ValidationsHolder(challengeRatingSchema, challengeRatingUpdateSchema);
     }
+    
     protected initializeRoutes(): void {
         //example route to add 
         // this.router.post(this.path+"/:id/response", this.submitResponse.bind(this));
